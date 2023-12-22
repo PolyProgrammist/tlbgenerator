e37d18bf84df79dd705b0c34f56efd961348a3db
polyprogrammist <vad.e.volodin@gmail.com>
2023-12-21 21:22:17 -0500

    tmp

--- src/codegen/generators/typescript/generator.ts
+++ src/codegen/generators/typescript/generator.ts
@@ -1,5 +1,5 @@
 import { TLBCode, TLBField, TLBType } from "../../ast";
-import { handleField } from "./field_handler";
+import { handleField } from "./handle_field";
 import { firstLower, getStringDeclaration, getSubStructName, goodVariableName } from "../../utils";
 import { CodeBuilder } from "../CodeBuilder";
 import { CodeGenerator } from "../generator";
--- src/codegen/generators/typescript/handle_field.ts
+++ src/codegen/generators/typescript/handle_field.ts
@@ -3,7 +3,7 @@ import { tIdentifier, tArrowFunctionExpression, tArrowFunctionType, tBinaryExpre
 import { TLBMathExpr, TLBVarExpr, TLBNumberExpr, TLBBinaryOp, TLBCode, TLBType, TLBConstructor, TLBParameter, TLBVariable, TLBField } from '../../ast'
 import { Expression, Statement, Identifier, BinaryExpression, ASTNode, TypeExpression, TypeParametersExpression, ObjectProperty, TypedIdentifier } from './tsgen'
 import { fillConstructors, firstLower, getCurrentSlice, bitLen, convertToMathExpr, splitForTypeValue, deriveMathExpression } from '../../utils'
-import { handleType } from './type_handler'
+import { handleType } from './handle_type'
 import { addLoadProperty, getNegationDerivationFunctionBody, getParamVarExpr, sliceLoad } from './utils'
 import { goodVariableName } from '../../utils'
 import { getType } from "../../astbuilder/handle_type"
